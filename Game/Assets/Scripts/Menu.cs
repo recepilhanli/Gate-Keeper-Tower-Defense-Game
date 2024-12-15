6348f56431ff2d18e9b41a5d7f6ea9b51a69e036
Hasan Gümüşten <hasom567567@icloud.com>
2024-12-15 16:57:48 +0300

    Play and quit button

---
+++ Game/Assets/Scripts/Menu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public void StartNextGame()
+    {
+        
+        int gameStartIndex = 1;
+
+        
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            SceneManager.LoadScene(gameStartIndex);
+        }
+        else
+        {
+        
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextSceneIndex = currentSceneIndex + 1;
+
+            if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings)
+            {
+                nextSceneIndex = 0; 
+            }
+
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+
+  
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); // Derlenmiş oyunu kapat
+#endif
+    }
+}
